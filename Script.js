89554a01a58f1ba3bd5d58413cfb78ed040ca778
sibroop <sibrooptalukdar.st@gmail.com>
2016-08-22 17:55:08 +0530

    JS5.4
    JS5.4

---
+++ Script.js
@@ -0,0 +1,11 @@
+function myFunction(a){
+    return a;
+}
+document.getElementById("print").innerHTML = myFunction("Hello World!");
+/*function myFuntion1(now){
+    return now;
+}
+var currentTime = new Date();
+
+document.write("today is "  +currentTime);*/
+
